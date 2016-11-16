e11a39b5cdb89cc04b0a10b6bb7feab40dbdad59
zhangchi <151250190@smail.nju.edu.cn>
2016-11-16 23:52:59 +0800

    ghjgk

---
+++ Hotel-parent/src/main/java/com/edu/nju/acm/Hotel_parent/App.java
@@ -0,0 +1,13 @@
+package com.edu.nju.acm.Hotel_parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
