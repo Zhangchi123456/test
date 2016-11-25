aaaa6dea3e3566d0a8fe0acc0ba89cd9059f7066
zwk <zwk@172.28.132.127>
2016-11-25 20:07:44 +0800

    main函数正确版。写了部分方法

--- Hotel-parent/Hotel-client/src/main/java/org/Hotel/client/presentation/main/Mainexample.java
+++ Hotel-parent/Hotel-client/src/main/java/org/Hotel/client/presentation/main/Mainexample.java
@@ -1,8 +1,8 @@
 package org.Hotel.client.presentation.main;
 
 import java.io.InputStream;
-import java.util.ResourceBundle;
 
+import java.util.ResourceBundle;
 
 
 import javafx.application.Application;
@@ -16,19 +16,23 @@ import javafx.stage.Stage;
 
 
 public class Mainexample extends Application{
-    private Stage mainStage;
+	private Stage mainStage;
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		BorderPane root=(BorderPane)FXMLLoader.load(getClass().getResource("/userui/Usermainui.fxml"));
-		Scene scene=new Scene(root);
-		primaryStage.setScene(scene);
+		init(primaryStage);
 		primaryStage.show();
 		
 	}
+	
+	//stage的初始化
     public void init(Stage stage){
-    	goto_Usermainui();
+    	this.mainStage=stage;
+    	goto_Loginui();
     }
+    
+    
+    //跳转到用户主界面
     public void goto_Usermainui(){
     	try {
 			replaceSceneContent("/userui/Usermainui.fxml");
@@ -38,15 +42,22 @@ public class Mainexample extends Application{
 		}
     }
     
+    //跳转到登录界面
+    public void goto_Loginui(){
+    	
+    }
+    
+    
+    //切换窗口的一个实现方法，用fxmlloader加载fxml文件路径再进行跳转
     private Initializable replaceSceneContent(String fxml) throws Exception {
 		FXMLLoader loader = new FXMLLoader();
 		InputStream in = Mainexample.class.getResourceAsStream(fxml);
 		loader.setBuilderFactory(new JavaFXBuilderFactory());
 		loader.setLocation(Mainexample.class.getResource(fxml));
 		
-		AnchorPane page;
+		BorderPane page;
 		try {
-			page = (AnchorPane) loader.load(in);
+			page = (BorderPane) loader.load(in);
 		}finally {
 			in.close();
 		} 
